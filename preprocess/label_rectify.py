299be31c81de6892988cc5463558ec8d47340ed3
szbonaldo <118182732+szbonaldo@users.noreply.github.com>
2024-06-27 15:25:00 +0800

    Update label_rectify.py

--- preprocess/label_rectify.py
+++ preprocess/label_rectify.py
@@ -9,15 +9,11 @@ csv_path = './Data_Entry_2017_v2020.csv'
 count = 1
 title = ['Image Index', 'Atelectasis', 'Cardiomegaly', 'Consolidation', 'Edema', 'Effusion', 'Emphysema', 'Fibrosis', 'Hernia', 'Infiltration', 'Mass', 'Nodule', 'Pleural_Thickening', 'Pneumonia', 'Pneumothorax']
 with open(csv_path, 'r') as csv_file:
-    # 读取csv文件
     csv_reader = csv.reader(csv_file)
 
-    # 创建一个新的csv文件，用于存储筛选后的结果
     with open('../onehot-label-PA.csv', 'w', newline='') as new_csv_file:
         csv_writer = csv.writer(new_csv_file)
-        # 遍历每一行数据
         for row in csv_reader:
-            # 如果行满足特定条件，则将其写入新文件
             if count == 1:
                 csv_writer.writerow(title)
                 count += 1
